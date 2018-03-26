77eac288076e8d4afce2fe3d0801ddeabaf8233c
user <user@350-03U.(none)>
2018-03-26 13:24:32 +0400

    hello word

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include <iostream>
-using namespace std;
-int main{
-	
+#include <stdlib.h>
+#include <curses.h>
+int main(void)
+{
+initscr();
+refresh();
+printw("Hello, World!");
+getch();
+endwin();
+return 0;
 }
